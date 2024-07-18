922fe1d701948ea6136ca2129889097d21335343
DianNguyxn <nguyendinhan0703@gmail.com>
2024-07-18 09:54:10 +0700

    completed

---
+++ destini-challenge-starting/android/app/src/main/java/co/appbrewery/destinichallengestarting/MainActivity.java
@@ -0,0 +1,7 @@
+package co.appbrewery.destinichallengestarting;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
+public class MainActivity extends FlutterActivity {
+}
